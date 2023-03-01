fc4a80680161bd978d5f91b96c7e8cef0c3b64d8
Ярослав Кисленков <Jarrpen@yandex.ru>
2023-03-01 23:40:54 +0300

    Homework after modification #1

--- homework.py
+++ homework.py
@@ -13,20 +13,18 @@ class InfoMessage:
         self.speed = speed
         self.calories = calories
 
-    def get_message(self):
-        return (f'Тип тренировки: {self.training_type}; '
-                f'Длительность: {self.duration:.3f} ч.; '
-                f'Дистанция: {self.distance:.3f} км; '
-                f'Ср. скорость: {self.speed:.3f}; '
-                f'Потрачено ккал: {self.calories:.3f}.')
+    def get_message(self) -> str:
+        return f'''Тип тренировки: {self.training_type};
+                Длительность: {self.duration:.3f} ч.;
+                Дистанция: {self.distance:.3f} км;
+                Ср. скорость: {self.speed:.3f};
+                Потрачено ккал: {self.calories:.3f}.'''
 
 
 class Training:
     """Базовый класс тренировки."""
-    LEN_STEPS: float = 0.65
+    LEN_STEP: float = 0.65
     M_IN_KM: int = 1000
-    CALORIES_MEAN_SPEED_MULTIPLIER: int = 18
-    CALORIES_MEAN_SPEED_SHIFT: float = 1.79
 
     def __init__(self,
                  action: int,
@@ -39,7 +37,7 @@ class Training:
 
     def get_distance(self) -> float:
         """Получить дистанцию в км."""
-        return self.action * self.LEN_STEPS / self.M_IN_KM
+        return self.action * self.LEN_STEP / self.M_IN_KM
 
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
@@ -64,6 +62,9 @@ class Training:
 
 class Running(Training):
     """Тренировка: бег."""
+    CALORIES_MEAN_SPEED_MULTIPLIER: int = 18
+    CALORIES_MEAN_SPEED_SHIFT: float = 1.79
+
     def __init__(self,
                  action: int,
                  duration: float,
@@ -77,8 +78,8 @@ class Running(Training):
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
         mean_speed = self.get_mean_speed()
-        return (self.CALORIES_MEAN_SPEED_MULTIPLIER
-                * (mean_speed + self.CALORIES_MEAN_SPEED_SHIFT)
+        return ((self.CALORIES_MEAN_SPEED_MULTIPLIER 
+                * mean_speed + self.CALORIES_MEAN_SPEED_SHIFT) 
                 * self.weight / self.M_IN_KM * self.duration)
 
 
@@ -86,6 +87,7 @@ class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
     WEIGHT_K1 = 0.035
     WEIGHT_K2 = 0.029
+    SPEED_K1 = 0.278
 
     def __init__(self,
                  action: int,
@@ -101,41 +103,42 @@ class SportsWalking(Training):
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
         mean_speed = self.get_mean_speed()
+        speed_in_ms = mean_speed * self.SPEED_K1
         return (self.WEIGHT_K1 * self.weight
-                + (mean_speed**2 / self.height)
+                + (speed_in_ms**2 / self.height)
                 * (self.WEIGHT_K2 * self.weight)
                 * self.duration)
 
 
 class Swimming(Training):
     """Тренировка: плавание."""
-    LEN_STEPS = 1.38
+    LEN_STEP = 1.38
     SPEED_K1 = 1.1
     SPEED_K2 = 2
 
     def __init__(self, action: int,
                  duration: float,
                  weight: float,
-                 lenght_pool: int,
+                 length_pool: int,
                  count_pool: int
                  ):
         super().__init__(action,
                          duration,
                          weight
                          )
-        self.lenght_pool = lenght_pool
+        self.length_pool = length_pool
         self.count_pool = count_pool
 
     def get_spent_calories(self) -> float:
         mean_speed = self.get_mean_speed()
-        return (mean_speed
-                + (self.SPEED_K1)
-                * (self.SPEED_K2)
-                * (self.weight)
+        return ((mean_speed
+                + self.SPEED_K1)
+                * self.SPEED_K2
+                * self.weight
                 * self.duration)
 
     def get_mean_speed(self) -> float:
-        return (self.lenght_pool
+        return (self.length_pool
                 * (self.count_pool)
                 / (self.M_IN_KM)
                 / self.duration)
